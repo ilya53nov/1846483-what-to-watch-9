71adca06c14e1a985270a465f5cc8fab501ea15a
ilya53nov <ilya53nov@yandex.ru>
2022-03-11 20:59:15 +0300

    Подключено действие

--- project/src/components/catalog-genres/genres-item.tsx
+++ project/src/components/catalog-genres/genres-item.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { useAppDispatch } from '../../hooks';
+import { choiceGenre } from '../../store/action';
 
 type GenresItemProps = {
   genre: string;
@@ -7,9 +9,17 @@ type GenresItemProps = {
 }
 
 export default function GenresItem({genre, activeGenre, setActiveGenre}: GenresItemProps):JSX.Element {
+  const dispatch = useAppDispatch();
+
   return(
     <li className={`catalog__genres-item${genre === activeGenre ? ' catalog__genres-item--active' : ''}`}>
-      <Link onClick={() => setActiveGenre(genre)} to='/' className="catalog__genres-link">{genre}</Link>
+      <Link onClick={() => {
+        dispatch(choiceGenre(genre));
+        setActiveGenre(genre);
+      }}
+      to='/' className="catalog__genres-link"
+      >{genre}
+      </Link>
     </li>
   );
 }
