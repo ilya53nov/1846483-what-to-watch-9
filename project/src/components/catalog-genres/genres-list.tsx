b9217db67fea9c4d337b93400591571141e98d97
ilya53nov <ilya53nov@yandex.ru>
2022-03-11 20:58:29 +0300

    Активный жанр берётся из хранилища

--- project/src/components/catalog-genres/genres-list.tsx
+++ project/src/components/catalog-genres/genres-list.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import { useAppSelector } from '../../hooks';
 import GenresItem from './genres-item';
-import { genres } from '../../const';
 
-const DEFAULT_GENRE = 'All genres';
+type GenresListProps = {
+  genres: string[];
+}
 
-export default function GenresList():JSX.Element{
-  const [activeItem, setActiveItem] = useState(DEFAULT_GENRE);
+export default function GenresList({genres}: GenresListProps):JSX.Element{
+  const activeGenre = useAppSelector((state) => state.genre);
+
+  const [activeItem, setActiveItem] = useState(activeGenre);
 
   const genresList = genres.map((genre) => <GenresItem key={genre} genre={genre} setActiveGenre={setActiveItem} activeGenre={activeItem} />);
 
