1c106bebab9c5bbe01a1d0957a3aab22eb3fc000
ilya53nov <ilya53nov@yandex.ru>
2022-03-11 20:53:03 +0300

    Подключен redux к проекту

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { films } from './mocks/films';
 import { comments } from './mocks/comments';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      films={films}
-      comments={comments}
-    />
+    <Provider store={store}>
+      <App
+        films={films}
+        comments={comments}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
