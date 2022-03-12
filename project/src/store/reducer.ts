91b1bbdf891574eb04ed84c2b185b650110b04bc
ilya53nov <ilya53nov@yandex.ru>
2022-03-13 00:58:33 +0300

    Добавлены действия в reducer

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,11 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { DEFAULT_GENRE, MAX_GENRES } from '../const';
+import { DEFAULT_GENRE, INITIAL_SHOW_FILM_COUNT, MAX_GENRES } from '../const';
 import { films } from '../mocks/films';
-import { choiceGenre } from './action';
+import { choiceGenre, resetShowedFilmsCount, showMore } from './action';
 
 const initialState = {
   genre: DEFAULT_GENRE,
   initialFilms: films,
+  showedFilmsCount: INITIAL_SHOW_FILM_COUNT,
   films: films,
   genres: [...new Set([DEFAULT_GENRE, ...Array.from(films, ({genre}) => genre)])].slice(0, MAX_GENRES),
 };
@@ -13,7 +14,14 @@ const initialState = {
 export const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(choiceGenre, (state, action) => {
+      state.showedFilmsCount = INITIAL_SHOW_FILM_COUNT;
       state.genre = action.payload;
       state.films = state.genre === DEFAULT_GENRE ? state.initialFilms : state.initialFilms.filter((film) => film.genre === state.genre);
+    })
+    .addCase(showMore, (state) => {
+      state.showedFilmsCount = state.showedFilmsCount + INITIAL_SHOW_FILM_COUNT;
+    })
+    .addCase(resetShowedFilmsCount, (state) => {
+      state.showedFilmsCount = INITIAL_SHOW_FILM_COUNT;
     });
 });
