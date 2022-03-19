4aa81a2ab826d8e9f062215c2ac916f663c4c224
ilya53nov <ilya53nov@yandex.ru>
2022-03-19 13:57:29 +0300

    Добавлена проверка авторизации пользователя и отображение экрана в соответствии со статусом авторизации

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -1,15 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { logoutAction } from '../../store/api-actions';
 
 export default function UserBlock(): JSX.Element {
+  const {authorizationStatus, data} = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    dispatch(logoutAction());
+    navigate(AppRoute.Main);
+  };
+
+  if (authorizationStatus === AuthorizationStatus.NoAuth || !data) {
+    return (
+      <div className="user-block">
+        <Link to={AppRoute.SignIn} className="user-block__link">Sign in</Link>
+      </div>
+    );
+  }
+
   return (
     <ul className="user-block">
       <li className="user-block__item">
         <div className="user-block__avatar">
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          <img src={data.avatarUrl} alt="User avatar" width="63" height="63" />
         </div>
       </li>
       <li className="user-block__item">
-        <Link to='/' className="user-block__link">Sign out</Link>
+        <Link to='/' onClick={onClick} className="user-block__link">Sign out</Link>
       </li>
     </ul>
   );
