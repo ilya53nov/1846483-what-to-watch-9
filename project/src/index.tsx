c7a5c05121b8301a8c6205354f45393f997cb1d9
ilya53nov <ilya53nov@yandex.ru>
2022-03-19 13:59:14 +0300

    Осуществлена загрузка с сервера промо фильма, фильма и статуса авторизации

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import { films } from './mocks/films';
-import { comments } from './mocks/comments';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter } from 'react-router-dom';
+import ErrorMessage from './components/error-message/errorMessage';
+import { checkAuthAction, fetchFilmAction, fetchFilmPromoAction } from './store/api-actions';
+
+store.dispatch(fetchFilmAction());
+store.dispatch(fetchFilmPromoAction());
+store.dispatch(checkAuthAction());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App
-          films={films}
-          comments={comments}
-        />
+        <ErrorMessage />
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
