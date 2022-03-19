9c55aa840cf14aac560b74d364f75d94e25fae16
ilya53nov <ilya53nov@yandex.ru>
2022-03-19 14:05:37 +0300

    Изменён экран приватного маршрута

--- project/src/private-route.tsx
+++ project/src/private-route.tsx
@@ -1,13 +1,14 @@
 import { Navigate, RouteProps } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from './const';
+import { useAppSelector } from './hooks';
 
 type PrivateRouteProps = RouteProps & {
-  authorizationStatus: AuthorizationStatus;
   children: JSX.Element;
 }
 
 export default function PrivateRoute(props: PrivateRouteProps): JSX.Element {
-  const {authorizationStatus, children} = props;
+  const {authorizationStatus} = useAppSelector((state) => state.user);
+  const {children} = props;
 
   return (
     authorizationStatus === AuthorizationStatus.Auth
