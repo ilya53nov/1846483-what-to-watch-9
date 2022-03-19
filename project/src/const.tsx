38f1a8ba2c1d9a3311eabd2f940cfa85a925b21a
ilya53nov <ilya53nov@yandex.ru>
2022-03-19 13:58:04 +0300

    Добавлены константы

--- project/src/const.tsx
+++ project/src/const.tsx
@@ -17,6 +17,16 @@ export enum Action {
   ChoiceGenre = 'Choice genre',
   ShowMore = 'Show more',
   ResetShowedFilmsCount = 'Reset showed films count',
+  RequireAuthorization = 'Require authorization',
+  SetError = 'Set error',
+  LoadFilms = 'Load films',
+  LoadSimilarFilms = 'Load similar films',
+  LoadFilm = 'Load film',
+  LoadPromoFilm = 'Load promo film',
+  LoadComments = 'Load comments',
+  Login = 'Login',
+  Logout = 'Logout',
+  AddComment = 'Add comment',
 }
 
 export enum Tab {
@@ -25,8 +35,42 @@ export enum Tab {
   Reviews = 'Reviews',
 }
 
+export enum APIRoute {
+  Login = '/login',
+  Logout = '/logout',
+  Films = '/films',
+  PromoFilm = '/promo',
+  Favorite = '/favorite',
+  Comments = '/comments',
+  Similar = '/similar',
+}
+
+export enum HTTP_CODE {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+}
+
+export const BACKEND_URL = 'https://9.react.pages.academy/wtw';
+
+export const REQUEST_TIMEOUT = 5000;
+
+export const TIMEOUT_SHOW_ERROR = 2000;
+
 export const DEFAULT_GENRE = 'All genres';
 
 export const MAX_GENRES = 9;
 
 export const FILMS_PER_PAGE = 8;
+
+export const MAX_RATING = 10;
+
+export const MIN_RATING = 1;
+
+export const DEFAULT_RATING = 8;
+
+export const DEFAULT_COMMENT = '';
+
+export const DEFAULT_MUTE = true;
+
+export const WAIT_TIME_MS = 1000;
