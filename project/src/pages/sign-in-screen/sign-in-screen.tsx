c256974c234466dc8712eb6c4ebe0b8538bb736a
ilya53nov <ilya53nov@yandex.ru>
2022-03-19 14:07:57 +0300

    Реализована возможность авторизации

--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,14 +1,43 @@
+import { FormEvent, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import { useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
+import { AuthData } from '../../types/auth-data';
+
 export default function SignInScreen():JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+      navigate(AppRoute.Main);
+    }
+  };
+
   return(
     <div className="sign-in user-page__content">
-      <form action="#" className="sign-in__form">
+      <form action="#" onSubmit={handleSubmit} className="sign-in__form">
         <div className="sign-in__fields">
           <div className="sign-in__field">
-            <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+            <input className="sign-in__input" ref={loginRef} type="email" placeholder="Email address" name="user-email" id="user-email" />
             <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
           </div>
           <div className="sign-in__field">
-            <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+            <input className="sign-in__input" ref={passwordRef} type="password" placeholder="Password" name="user-password" id="user-password" />
             <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
           </div>
         </div>
