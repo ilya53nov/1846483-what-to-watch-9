5acdb4685bfd7af5d4d7a4d94865653558678589
ilya53nov <ilya53nov@yandex.ru>
2022-03-28 12:51:00 +0300

    Добавлены типы для слайсов reducer

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,5 +1,52 @@
+import { AuthorizationStatus } from '../const';
 import { store } from '../store/index';
+import { Comments } from './comment';
+import { Film, Films } from './film';
+import { UserData } from './user-data';
 
+export type UserProcess = {
+  authorizationStatus: AuthorizationStatus;
+  data: UserData;
+}
+
+export type AppData = {
+  films: {
+    data: Films;
+    isLoaded: boolean;
+    filteredFilmsByGenre: Films;
+  }
+
+  fovoriteFilms: {
+    data: Films;
+    isLoaded: boolean;
+  }
+
+  film: {
+    data: Film;
+    isLoaded: boolean;
+    similarFilms: Films;
+    isFound: boolean | Unknown;
+
+    comments: {
+      data: Comments;
+      isLoaded: boolean;
+    };
+  }
+
+  initialFilms: Films;
+
+  promoFilm: Film;
+
+  error: string;
+}
+
+export type AppProcess = {
+  activeGenre: string;
+  showedFilmsCount: number;
+  filteredFilmsByGenre: Films;
+}
+
+export type Unknown = 'UNKNOWN';
 
 export type State = ReturnType<typeof store.getState>;
 
