f011c4a1a38c999adf37f575cbb97425a4e55140
ilya53nov <ilya53nov@yandex.ru>
2022-03-28 12:46:59 +0300

    Добавлено действие фильтрации фильмов по жанру

--- project/src/store/app-process/app-process.ts
+++ project/src/store/app-process/app-process.ts
@@ -1,9 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { DEFAULT_GENRE, FILMS_PER_PAGE, NameSpace } from '../../const';
-//import { useAppSelector } from '../../hooks';
 import { Film, Films } from '../../types/film';
 import { AppProcess } from '../../types/state';
-//import { getInitialFilms } from '../app-data/selectors';
 
 const initialState: AppProcess = {
   activeGenre: DEFAULT_GENRE,
@@ -16,13 +14,7 @@ export const appProcess = createSlice({
   initialState,
   reducers: {
     filterFilmsByGenre: (state, action) => {
-      //const films = action.payload;
       state.filteredFilmsByGenre = state.activeGenre === DEFAULT_GENRE ? action.payload : action.payload.filter((film: Film) => film.genre === state.activeGenre);
-
-      console.log(state.filteredFilmsByGenre, 'state');
-      //const initialFilms = getInitialFilms({DATA}: appData);
-      //console.log();
-
     },
     choiceGenre: (state, action) => {
       state.showedFilmsCount = FILMS_PER_PAGE;
