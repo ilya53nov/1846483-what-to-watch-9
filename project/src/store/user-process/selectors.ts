f6b97bb0d9026650931bdb4c8b92b1dab1b1ae10
ilya53nov <ilya53nov@yandex.ru>
2022-04-03 22:04:08 +0300

    Рефакторинг

--- project/src/store/user-process/selectors.ts
+++ project/src/store/user-process/selectors.ts
@@ -2,6 +2,6 @@ import { AuthorizationStatus, NameSpace } from '../../const';
 import { State } from '../../types/state';
 import { UserData } from '../../types/user-data';
 
-export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.user].authorizationStatus;
+export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.User].authorizationStatus;
 
-export const getUserData = (state: State): UserData => state[NameSpace.user].data;
+export const getUserData = (state: State): UserData => state[NameSpace.User].data;
