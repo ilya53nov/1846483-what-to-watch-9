204097c768e58051e01ab706a111cd0ce4bc1b78
ilya53nov <ilya53nov@yandex.ru>
2022-04-03 22:00:19 +0300

    Добавлена прослойка для редиректа

---
+++ project/src/store/middlewares/redirect.ts
@@ -0,0 +1,17 @@
+import browserHistory from '../../browser-history';
+import {Middleware} from 'redux';
+import {rootReducer} from '../root-reducer';
+import { Action } from '../../const';
+
+type Reducer = ReturnType<typeof rootReducer>;
+
+export const redirect: Middleware<unknown, Reducer>=
+  (_store) =>
+    (next) =>
+      (action) => {
+        if (action.type === Action.Redirect) {
+          browserHistory.push(action.payload);
+        }
+
+        return next(action);
+      };
